cf27a653a164dc63762bcdbc97fe07d5ea7bda44
KevinC93 <kevincacheiro93@gmail.com>
2025-09-22 19:37:02 -0400

    Delete components directory

--- components/Section.tsx
+++
@@ -1,8 +0,0 @@
-export default function Section({ id, title, children }: { id?: string; title: string; children: React.ReactNode }) {
-return (
-<section id={id} className="container py-12">
-<h2 className="h2">{title}</h2>
-{children}
-</section>
-)
-}
