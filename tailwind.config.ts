7326fbb99e6504df1c84e15751e5612d7372f3ee
KevinC93 <kevincacheiro93@gmail.com>
2025-09-22 16:11:05 -0400

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'tailwindcss'
+
+
+export default {
+content: [
+'./app/**/*.{js,ts,jsx,tsx}',
+'./components/**/*.{js,ts,jsx,tsx}',
+],
+theme: {
+extend: {
+colors: {
+brand: {
+blue: '#00aaff',
+black: '#0a0a0a',
+red: '#ff2a2a'
+}
+},
+fontFamily: {
+display: ['var(--font-space-grotesk)'],
+body: ['var(--font-manrope)']
+},
+},
+},
+plugins: [],
+} satisfies Config
